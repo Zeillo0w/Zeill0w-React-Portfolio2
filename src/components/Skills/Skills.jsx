27928bc3a749fd7848908351d1944cd00c3757b7
ZΞiLL0w <yanis.ghanem@ynov.com>
2024-02-01 15:40:44 +0100

    Skills info test

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Skills.css";
 import { SKILLS } from "../../utils/data";
-import Skillcard from "./Skillcard";
+import Skillcard from "./SkillCard/SkillCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
 
 
 const Skills = () => {
+    const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+    const handleSelectedSkill = (data) => {
+        setSelectedSkill(data);
+    };
+
+
     return (
         <section className="skills-container">
             <h5>Site en cours de developpement !</h5>
@@ -17,13 +25,20 @@ const Skills = () => {
                             key={item.title}
                             iconUrl={item.icon}
                             title={item.title}
+                            isActive={selectedSkill.title === item.title}
+                            onClick={() => {
+                                handleSelectedSkill(item);
+                            }}
                         />
 
                     ))}
                 </div>
 
                 <div className="skills-info"></div>
-
+                <SkillsInfoCard
+                    heading={selectedSkill.title}
+                    skills={selectedSkill.skills}
+                />
             </div>
 
         </section>
